a31582a4530fbc0f8428c273712beb5be2ba6be0
ethanfire <73967680+ethanfire@users.noreply.github.com>
2021-12-09 00:39:44 -0500

    Add files via upload
    It works now maybe

--- MatrixGraph.cpp
+++ MatrixGraph.cpp
@@ -7,24 +7,46 @@ MatrixGraph::MatrixGraph(std::unordered_map<std::string, std::set<std::string>>&
 
 void MatrixGraph::InitializeGraph(std::unordered_map<std::string, std::set<std::string>>& matrixList)
 {
-	int i = 0;
-	//Iterate through the map and add each key to the mapper
+	int i = 0; //keeps track of index for mapper
+
+	//Iterate through the map and add each key to the mapper, if the key hasn't been added before, add the original artists' adj list too
 	for (auto iter = matrixList.begin(); iter != matrixList.end(); ++iter)
 	{
-		//add artist to mapper
-		mapper[iter->first] = i;
-		i++;
+		//add artist to mapper, if they aren't already in there
+		if (mapper.count(iter->first) == 0)
+		{
+			mapper[iter->first] = i;
+			i++;
+
+			//add their adjacent babies from their set now (iterate through the set found at the current artist's key)
+			for (auto subIter = matrixList[iter->first].begin(); subIter != matrixList[iter->first].end(); ++subIter)
+			{
+				//if the adj artist isn't in the list already, add them
+				if (mapper.count(*subIter) == 0)
+				{
+					mapper[*subIter] = i;
+					i++;
+				}
+			}
+		}
 	}
 
-	//iterate through the map and change the adjacent artists to "true" at each index
-	for (auto iter = matrixList.begin(); iter != matrixList.end(); ++iter)
-	{
-		int currArtist = mapper[iter->first];
+	//Everyone is now in the mapper, but not in the matrix 2D vector, now we'll add them
 
-		//to do this, we will iterate through the adjacent sets
-		for (auto it = matrixList[iter->first].begin(); it != matrixList[iter->first].end(); ++it)
+	//initialize vector and set size to the size of mapper, set values to false
+	matrix.clear(); //double check it's completely empty
+	matrix.resize(mapper.size(), std::vector<bool>(mapper.size(), false));
+
+
+	//iterate through the mapper and change the adjacent artists to "true" at each index
+	for (auto iter = mapper.begin(); iter != mapper.end(); ++iter)
+	{
+		int currArtist = mapper[iter->first]; //gives index of current artist we're iterating on
+
+		//iterate through the adjacent set
+		for (auto subIter = matrixList[iter->first].begin(); subIter != matrixList[iter->first].end(); ++subIter)
 		{
-			int adjArtist = mapper[*it];
+			int adjArtist = mapper[*subIter];
 
 			if (adjArtist == currArtist)	//take out the fact that an artist appears in their own adjacency list
 				continue;
@@ -43,7 +65,7 @@ bool MatrixGraph::CheckArtist(std::string artist) //check the mapper to see if a
 		return false;
 }
 	
-std::set<std::string> MatrixGraph::GetAdjacents(std::string artist) //return a vector that has all the "True" elements in the vector at the given index
+std::set<std::string> MatrixGraph::getAdjacents(std::string artist) //return a vector that has all the "True" elements in the vector at the given index
 {
 	int wantedArtist = mapper[artist];	//index of the artist we want the adjacent artists of
 	std::set<std::string> adjList;
@@ -54,7 +76,7 @@ std::set<std::string> MatrixGraph::GetAdjacents(std::string artist) //return a v
 		int currArtist = iter->second;	//index of the current artist in the iteration
 
 		if (matrix[wantedArtist][currArtist])	//if the value at the indices of the current artist and the wanted artist are true, meaning adjacent. . .
-			adjList.insert(iter->first);		//push the value of the current artist into the vector to be returned
+			adjList.insert(iter->first);		//push the value of the current artist into the set to be returned
 	}
 
 	return adjList;
